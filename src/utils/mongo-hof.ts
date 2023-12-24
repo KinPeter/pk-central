f21597934095a2efd37fe9e02147f2418959cf0b
Peter Kin <kinpeter85@gmail.com>
2023-12-24 10:03:19 +0200

    Refactor mongo hof

--- src/utils/mongo-hof.ts
+++ src/utils/mongo-hof.ts
@@ -2,30 +2,33 @@ import { Db, MongoClient, ServerApiVersion } from 'mongodb';
 import { Context } from '@netlify/functions';
 import { ErrorResponse } from './response.js';
 
-const client = new MongoClient(process.env.MONGO_DB_URI ?? '', {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-const connectionPromise = client.connect();
+// const connectionPromise = client.connect();
 
 export async function withMongoDb(
   nextStepFunction: (req: Request, context: Context, db: Db) => Promise<Response>,
   req: Request,
   context: Context
 ): Promise<Response> {
+  let client: MongoClient | null = null;
+
   try {
-    const db = (await connectionPromise).db(process.env.MONGO_DB_NAME);
-    console.log('Connected to the database');
+    client = new MongoClient(process.env.MONGO_DB_URI ?? '', {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      },
+    });
+    const db = (await client.connect()).db(process.env.MONGO_DB_NAME);
+    console.log('Connected to the database: ', db.databaseName);
     return await nextStepFunction(req, context, db);
   } catch (e) {
     return new ErrorResponse('Could not connect to the database', 500, e);
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
-    console.log('Closed connection to the database');
+    if (client) {
+      await client.close();
+      console.log('Closed connection to the database');
+    }
   }
 }
