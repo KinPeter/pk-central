483f3c3d3b860ca6a00803eaa7d36f63e20df221
Peter Kin <kinpeter85@gmail.com>
2024-02-04 15:34:06 +0100

    Reduce Jest workers

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,14 @@
 import type { JestConfigWithTsJest } from 'ts-jest';
 
 const jestConfig: JestConfigWithTsJest = {
-  // [...]
-  // Replace `ts-jest` with the preset you want to use
-  // from the above list
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleNameMapper: {
     '^pk-common$': '<rootDir>/common/index.ts$1',
   },
   testMatch: ['<rootDir>/test/**/*.spec.ts'],
+  maxWorkers: 1,
+  maxConcurrency: 1,
 };
 
 export default jestConfig;
